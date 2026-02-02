d7d8dbba17d219b33e5626aa12290832a44ec55c
khaihoan1504 <hoannzey@gmail.com>
2026-02-02 13:20:13 +0700

    ss4kt

--- Ex2.js
+++ Ex2.js
@@ -1,10 +1,12 @@
 let sum = 0;
 for (let i = 1; i <= 50; i++) {
-  if (i % 3 === 0) {
-    console.log("fizz");
-    sum += i;
+  if (i % 3 === 0 && i % 5 === 0) {
+    console.log("fizzbuzz");
   } else if (i % 5 === 0) {
     console.log("buzz");
+  } else if (i % 3 === 0) {
+    console.log("fizz");
+    sum += i;
   } else {
     console.log(i);
   }
